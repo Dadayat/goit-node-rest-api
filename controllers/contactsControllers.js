684fb02a360d761ec3c60ddaa08a5f41587bb614
Dadayat <136356333+Dadayat@users.noreply.github.com>
2024-05-07 16:51:25 +0300

    add get, delete function

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,10 +1,39 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await contactsService.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsService.getContactById(id);
+    if (!contact) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsService.removeContact(id);
+    if (!contact) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const createContact = (req, res) => {};
 
