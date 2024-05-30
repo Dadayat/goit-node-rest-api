66082fbc4364c0f9725ba786c94b943516cda5b5
Dadayat <136356333+Dadayat@users.noreply.github.com>
2024-05-30 20:50:03 +0300

    add changes again

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -4,9 +4,10 @@ import { emailRegexp } from "../models/user.js";
 export const registerSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().required().min(6),
+    subscription: Joi.string().required(),
   });
   
-  export const loginSchema = Joi.object({
+export const loginSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().required().min(6),
   });
