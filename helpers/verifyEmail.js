1ed60f6865586344cf82918c1bc61c225576a52e
Dadayat <136356333+Dadayat@users.noreply.github.com>
2024-06-02 12:50:25 +0300

    change email

--- helpers/verifyEmail.js
+++ helpers/verifyEmail.js
@@ -3,7 +3,7 @@ import sendEmail from '../helpers/sendEmail.js';
 export async function sendVerificationEmail(email, verificationToken) {
     const verifyEmail = {
       to: email,
-      from: 'dadayat@meta.ua',
+      from: 'dashemiakina@gmail.com',
       subject: "Welcome to phonebook",
       http: `<a target="_blank" href="${process.env.BASE_URL}/api/users/verify/${verificationToken}">Click to verify email</a>`,
       text: `To confirm your registration please open the link ${process.env.BASE_URL}/api/users/verify/${verificationToken}`,
